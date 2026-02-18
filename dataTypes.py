88c7a4de37dccd267b25b2aaa0834a299e232dcf
mjamosk254 <amoskaraniwachira001@gmail.com>
2026-02-18 10:55:48 +0300

    python basics

---
+++ dataTypes.py
@@ -0,0 +1,35 @@
+#string-it is a collection of characters(a-z)""
+name="amos"
+print(name)
+location="nairobi"
+print(location)
+job="researcher"
+print(job)
+#integer-whole numbers
+age=20
+print(age)
+amount=1000
+print(amount)
+#float-decimal numbers
+height=5.7
+print(height)
+#boolean-true or false
+is_adult=True
+print(is_adult)
+is_online=False
+print(is_online)
+#list-is a collection of items,it can be changed
+fruits=["mangoes","apples","oranges"]
+print(fruits)
+marks=[51,97,81,76]
+print(marks)
+#tuple-it is a collection of items, it cannot be changed
+adm_numbers=(67,7569,2695)
+print(adm_numbers)
+#dictionary(dict)-it contains a key and a value as a pair
+student={
+    "name":"amos",
+    "age":25,
+    "occupation":"researcher"
+}
+print(student)
---
+++ string.py
@@ -0,0 +1,11 @@
+first_name="john"
+last_name="duo"
+full_name=first_name + " " + last_name
+print(full_name)
+#f-string method
+name="amos"
+age=25
+print(f"hello my name is {name} and I am {age} years old")
+#.format
+location="mombasa"
+print("i live in {}".format(location))
