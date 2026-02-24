dc6b009f2305061706e2123f1b11cdb2b1b39594
mjamosk254 <amoskaraniwachira001@gmail.com>
2026-02-24 11:12:35 +0300

    loop

---
+++ loop.py
@@ -0,0 +1,26 @@
+
+#for loop
+for i in range(20): #i can also be represented by any other valuable eg. x,y etc
+    print("welcome")
+for x in range(100):
+    print(x)   
+#custome range
+for y in range(1,6):
+    print(y) 
+products=["laptop","mouse","phone","television"]
+print(products)
+for product in products:
+    print(product)
+students=["mary","john","ann","joseph"]
+for student in students:
+    print("hello",student)
+
+#while loop-it runs as long the condition is true
+count=10
+while count <=20:
+    print(count)
+    count+=1
+number=2
+while number<=100:
+    print(number)
+    number+=2        
